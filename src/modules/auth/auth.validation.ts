48509819d8f34a39d0244b3218c47280dcff81e2
arindam01gi <arindammaiti2018@gmail.com>
2026-06-03 09:08:43 +0530

    feat: validation added for login and register

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -1,8 +1,24 @@
-import { z } from 'zod';
+ import { z } from 'zod';
 
-// Build your structural runtime schema checks here
-export const authSchema = z.object({
-  body: z.object({
-    // field: z.string()
-  }),
-});
+ export const resgisterSchema = z.object({
+  body : z.object({
+     email : z.string().email('Invalid email address format'),
+     password: z.string().min(6, `Password must be minimum 6 letters`),
+     phone: z.string().min(10, 'Phone no must be 10 digits')
+  })
+ })
+
+export const emailLoginSchema = z.object({
+  body:z.object({
+     email : z.string().email('Invalid email address format'),
+     password: z.string().min(6, `Password must be minimum 6 letters`),
+  })
+})
+
+
+export const phoneLoginSchema = z.object({
+  body:z.object({
+     phone: z.string().min(10, 'Phone no must be 10 digits'),
+     password: z.string().min(6, `Password must be minimum 6 letters`),
+  })
+})
